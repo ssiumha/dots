c7efcadc3bc27f5f16d924ba6e8efadb763081d8
hassium16 <hassium16@gmail.com>
2017-02-13 16:40:17 +0900

    bootstrap에 .local 폴더 초기화와 fzy 설치를 추가

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,10 @@
 [[ ! -s "$HOME/.vimrc" ]] && echo "source ~/dotfiles/vimrc" > "$HOME/.vimrc"
 [[ ! -s "$HOME/.zshrc" ]] && echo "source ~/dotfiles/zshrc" > "$HOME/.zshrc"
 
+mkdir -p "$HOME/.local" "$HOME/.local/repo" "$HOME/.local/bin" "$HOME/.local/vim"
 
 [[ ! -s "$HOME/.local/bin/diff-highlight" ]] && \
     wget "https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight/diff-highlight" "$HOME/.local/bin/diff-highlight"
+
+[[ ! -s "$HOME/.local/bin/fzy" ]] && \
+    git clone "https://github.com/jhawthorn/fzy.git" "$HOME/.local/repo/fzy" && cd "$HOME/.local/repo/fzy" && make && make install -e "PREFIX=$HOME/.local/" && cd -
