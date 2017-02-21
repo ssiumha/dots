3bebd447f2fbe4912a2e39557135114972a3dcf6
hassium16 <hassium16@gmail.com>
2017-02-21 13:56:08 +0900

    [tmux] 환경에 따른 조건 분기가 필요한 설정을 tmux.sh로 빼냄
    tmux.conf 안에서 처리할 경우 설정이 더러워지고 예외도 발생함

---
+++ tmux.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [[ `tmux -V | awk 'END{if($2>=1.9)exit 0;exit 1}'` ]]; then
+  tmux -- \
+    bind c new-window -c "#{pane_current_path}" \; \
+    bind \" split-window -v -c "#{pane_current_path}" \; \
+    bind % split-window -h -c "#{pane_current_path}" \; \
+  ;
+else
+  tmux -- \
+    bind c new-window \; \
+    bind \" split-window -v \; \
+    bind % split-window -h \; \
+  ;
+fi
+
+
+if [[ -z $SSH_CLIENT ]]; then
+  tmux -- \
+    set -sq status-right '#{?client_prefix,#[reverse],}' \; \
+    set -sqa status-right '#(whoami)@#h' \; \
+    set -sqa status-right '#[fg=colour15]|' \; \
+    set -sqa status-right '#[fg=colour14]%y-%m-%d(%a) %H:%M' \; \
+  ;
+else
+  tmux -- \
+    set -sq status-right '#{?client_prefix,#[reverse],}' \; \
+    set -sqa status-right '#[fg=colour3]#(whoami)@#h' \; \
+  ;
+fi
+
+exit 0;
