3dc4b6180cae20102005abfdc1d5a3665c0f318e
hassium16 <hassium16@gmail.com>
2017-03-22 23:42:57 +0900

    [emacs] init.el 추가

---
+++ emacs/init.el
@@ -0,0 +1,19 @@
+;; install)
+;; mkdir -p ~/.emacs.d && echo '(load-file "~/dotfiles/emacs/init.el")' > ~/.emacs.d/init.el
+
+(prefer-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8-unix)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(dolist (package '(
+		   auto-complete ace-jump-mode
+		   ))
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
