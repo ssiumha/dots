d73351e40db31f59cf8a73c9df1bc97bd85d5b28
hassium16 <hassium16@gmail.com>
2017-03-23 13:20:33 +0900

    [emacs] ido mode 라이브러리 추가

--- emacs/init.el
+++ emacs/init.el
@@ -6,6 +6,9 @@
 
 (tool-bar-mode -1)
 
+(require 'ido)
+(ido-mode t)
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
