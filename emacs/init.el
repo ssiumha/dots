baee8607e375d524236b35ca5942730fda1bb293
hassium16 <hassium16@gmail.com>
2017-04-03 15:08:28 +0900

    [emacs] adoc header 크기 조정

--- emacs/init.el
+++ emacs/init.el
@@ -80,6 +80,17 @@
 
 ;; adoc-mode
 (add-to-list 'auto-mode-alist (cons "\\.adoc\\'" 'adoc-mode))
+(add-hook 'adoc-mode-hook (lambda()
+                            (set-face-attribute 'markup-title-0-face nil :height 1.5)
+                            (set-face-attribute 'markup-title-1-face nil :height 1.4)
+                            (set-face-attribute 'markup-title-2-face nil :height 1.3)
+                            (set-face-attribute 'markup-title-3-face nil :height 1.2)
+                            (set-face-attribute 'markup-title-4-face nil :height 1.1)
+                            (set-face-attribute 'markup-title-5-face nil :height 1.0)
+                            (set-face-attribute 'markup-meta-face nil :height 1.0)
+                            (set-face-attribute 'markup-meta-hide-face nil :height 1.0)
+                            ))
+
 
 
 ;; guide-key
