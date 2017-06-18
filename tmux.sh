c5c109335f03ccb9c2879476150dcaaee2392d12
hassium16 <hassium16@gmail.com>
2017-06-18 16:55:53 +0900

    [tmux] 2.4 업데이트로 bindkey 설정 터지는 문제 수정
    1. bind-key -t 옵션이 -T로 수정됨
    2. key table 이름이 vi-copy에서 copy-mode-vi로 수정됨
    3. 키 정의 부분에 send -X를 붙임

--- tmux.sh
+++ tmux.sh
@@ -15,6 +15,49 @@ else
 fi
 
 
+if [[ `tmux -V | awk 'END{print ($2>=2.4)}'` -eq 1 ]]; then
+  tmux -- \
+    bind -T copy-mode-vi Escape send -X clear-selection \; \
+    bind -T copy-mode-vi C-v    send -X rectangle-toggle \; \
+    bind -T copy-mode-vi v      send -X begin-selection \; \
+    bind -T copy-mode-vi V      send -X select-line \; \
+    bind -T copy-mode-vi C-a    send -X start-of-line \; \
+    bind -T copy-mode-vi C-e    send -X end-of-line \; \
+    bind -T copy-mode-vi w      send -X next-word \; \
+    bind -T copy-mode-vi e      send -X next-word-end \; \
+    bind -T copy-mode-vi b      send -X previous-word \; \
+    bind -T copy-mode-vi /      send -X search-forward \; \
+    bind -T copy-mode-vi ?      send -X search-backward \; \
+    bind -T copy-mode-vi C-b    send -X page-up \; \
+    bind -T copy-mode-vi C-f    send -X page-down \; \
+    bind -T copy-mode-vi C-u    send -X halfpage-up \; \
+    bind -T copy-mode-vi C-d    send -X halfpage-down \; \
+    bind -T copy-mode-vi Enter copy-pipe "~/dotfiles/bin/clipcopy" \; \
+    bind -T copy-mode-vi y     copy-pipe "~/dotfiles/bin/clipcopy" \; \
+  ;
+else
+  tmux -- \
+    bind -t vi-copy Escape clear-selection \; \
+    bind -t vi-copy C-v    rectangle-toggle \; \
+    bind -t vi-copy v      begin-selection \; \
+    bind -t vi-copy V      select-line \; \
+    bind -t vi-copy C-a    start-of-line \; \
+    bind -t vi-copy C-e    end-of-line \; \
+    bind -t vi-copy w      next-word \; \
+    bind -t vi-copy e      next-word-end \; \
+    bind -t vi-copy b      previous-word \; \
+    bind -t vi-copy /      search-forward \; \
+    bind -t vi-copy ?      search-backward \; \
+    bind -t vi-copy C-b    page-up \; \
+    bind -t vi-copy C-f    page-down \; \
+    bind -t vi-copy C-u    halfpage-up \; \
+    bind -t vi-copy C-d    halfpage-down \; \
+    bind -t vi-copy Enter copy-pipe "~/dotfiles/bin/clipcopy" \; \
+    bind -t vi-copy y     copy-pipe "~/dotfiles/bin/clipcopy" \; \
+  ;
+fi
+
+
 if [[ -z $SSH_CLIENT ]]; then
   tmux -- \
     set -sq status-right '#{?client_prefix,#[reverse],}' \; \
