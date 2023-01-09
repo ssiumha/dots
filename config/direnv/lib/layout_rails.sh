83e0c78ea4557e5fd61913be43cfaf8dc3e0e1a9
ssiumha <ssiumha@gmail.com>
2023-01-09 20:08:45 +0900

    [direnv] update rails lib

--- config/direnv/lib/layout_rails.sh
+++ config/direnv/lib/layout_rails.sh
@@ -1,5 +1,6 @@
 layout_rails() {
-  PATH_add $(dirname $(find_up bin/rails))
+  PATH_add $(dirname ${BASH_SOURCE[0]})/rails;
 
+  # PATH_add $(dirname $(find_up bin/rails))
   # alias routes="bundle exec rails routes | fzf"
 }
