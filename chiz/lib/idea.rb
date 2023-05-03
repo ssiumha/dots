f6e0c1b4b82bf6a3a57a4cfd008895ee96e9293d
ssiumha <ssiumha@gmail.com>
2023-05-03 10:20:16 +0900

    [chiz] mv to spells

--- chiz/lib/idea.rb
+++
@@ -1,9 +0,0 @@
-module Lib
-  class IdeaChiz < Base
-    md :automation_path, 'ci, cd, etc..', <<~MD
-      ref: https://github.com/github/super-linter/tree/main/.automation
-
-      CI, CD, deploy에 관련된 파일은 .automation 폴더를 만들어서 관리하면 이름도 알맞고 깔끔하다
-    MD
-  end
-end
--- chiz/lib/tool.rb
+++
@@ -1,5 +0,0 @@
-module Lib
-  class ToolChiz < Base
-    md :asciinema, '#rust cli terminal recording', 'https://asciinema.org'
-  end
-end
