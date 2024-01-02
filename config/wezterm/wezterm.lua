d7b0333fa405419cb16607969acfce97c7d365e7
ssiumha <ssiumha@gmail.com>
2024-01-02 17:49:19 +0900

    [wezterm] set background opacity

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,6 +2,8 @@ local wezterm = require 'wezterm'
 
 -- ref: https://wezfurlong.org/wezterm/config/lua/config
 return {
+  window_background_opacity = 0.9,
+
   hide_tab_bar_if_only_one_tab = true,
   font_size = 12.0,
 
