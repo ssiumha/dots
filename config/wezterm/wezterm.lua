fa3b7a440458d0d72bf5ba0e4c901b08199d4adf
ssiumha <ssiumha@gmail.com>
2026-03-11 13:47:03 +0900

    [config] misc updates

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,7 +1,15 @@
 local wezterm = require 'wezterm'
 
+-- default rules + gh:owner/repo#NNN → GitHub PR URL
+local hyperlink_rules = wezterm.default_hyperlink_rules()
+table.insert(hyperlink_rules, {
+  regex = [[\bgh:([\w.-]+/[\w.-]+)#(\d+)]],
+  format = 'https://github.com/$1/pull/$2',
+})
+
 -- ref: https://wezfurlong.org/wezterm/config/lua/config
 return {
+  hyperlink_rules = hyperlink_rules,
   window_background_opacity = 0.9,
 
   hide_tab_bar_if_only_one_tab = true,
